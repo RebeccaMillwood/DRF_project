2b3aea87de8b497555e1eb5b9539deb3aef0e6f8
RebeccaMillwood <bec-millwood@live.com.au>
2020-09-15 20:34:02 +1000

    updated user model and user serializer, deleted database and made migrations after changing user model

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
-
+    first_name = models.TextField(max_length=30, blank=True)
+    last_name = models.TextField(max_length=30, blank=True)
+    location = models.TextField(max_length=30, blank=True)
+    image = models.TextField(default="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg")
+    about_me = models.TextField(max_length=500, blank=True)
+    
     def __str__(self):
         return self.username
 
--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -6,6 +6,11 @@ class CustomUserSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=200)
     email = serializers.CharField(max_length=200)
     password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(max_length=30)
+    last_name = serializers.CharField(max_length=30)
+    location = serializers.CharField(max_length=30)
+    image = serializers.CharField(default="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg")
+    about_me = serializers.CharField(max_length=500)
 
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
