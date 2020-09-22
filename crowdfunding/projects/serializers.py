df5ecc4ab941992fa15eec36d341e6dcddb09f74
RebeccaMillwood <bec-millwood@live.com.au>
2020-09-22 19:03:50 +1000

    added a comment

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -40,3 +40,4 @@ class ProjectDetailSerializer(ProjectSerializer):
             instance.owner = validated_data.get('owner', instance.owner)
             instance.save()
             return instance
+# need to maje a pledge detail here
