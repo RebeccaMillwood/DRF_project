ff03a77a5eef4882d827b4d3dd2cd2c1e439772b
RebeccaMillwood <bec-millwood@live.com.au>
2020-09-26 11:18:55 +1000

    edited date created in model and seializer to auto add the current date and time the project is created

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -27,10 +27,11 @@ class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length = 200)
     description = serializers.CharField(max_length = None)
+    location = serializers.CharField(max_length = None)
     goal = serializers.IntegerField()
     image = serializers.URLField()
     is_open = serializers.BooleanField()
-    date_created = serializers.DateTimeField()
+    date_created = serializers.DateTimeField(read_only=True)
     owner = serializers.ReadOnlyField(source='owner.id')
     # pledges = PledgeSerializer(many=True, read_only=True)
 
@@ -43,10 +44,11 @@ class ProjectDetailSerializer(ProjectSerializer):
         def update(self, instance, validated_data):
             instance.title = validated_data.get('title', instance.title)
             instance.description = validated_data.get('description', instance.description)
+            instance.location = validated_data.get('location', instance.location)
             instance.goal = validated_data.get('goal', instance.goal)
             instance.image = validated_data.get('image', instance.image)
             instance.is_open = validated_data.get('is_open', instance.is_open)
-            instance.date_created = validated_data.get('date_created', instance.date_created)
+            # instance.date_created = validated_data.get('date_created', instance.date_created)
             instance.owner = validated_data.get('owner', instance.owner)
             instance.save()
             return instance
