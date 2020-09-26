b93a1cac5ff8c4ed7736966869d93c1fe8dbd5b4
RebeccaMillwood <bec-millwood@live.com.au>
2020-09-26 12:07:39 +1000

    added permissions import to user views.py

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 from .permissions import IsSelfOrReadOnly
